DavidAlphaFox/epoch

[WIP] Add Stratum session handling

apps/aestratum/src/aestratum_handler.erl
@@ -21,36 +21,36 @@
           session
          }).
 
--define(IS_MSG(SM), ((SM =:= tcp) or (SM =:= ssl))).
--define(IS_CLOSE(SC), ((SC =:= tcp_closed) or (SC =:= ssl_closed))).
--define(IS_ERROR(SE), ((SE =:= tcp_error) or (SE =:= ssl_error))).
+-define(IS_MSG(T), ((T =:= tcp) or (T =:= ssl))).
+-define(IS_CLOSE(C), ((C =:= tcp_closed) or (C =:= ssl_closed))).
+-define(IS_ERROR(E), ((E =:= tcp_error) or (E =:= ssl_error))).
 
 start_link(Ref, Socket, Transport, Opts) ->
 	{ok, proc_lib:spawn_link(?MODULE, init, [{Ref, Socket, Transport, Opts}])}.
 
 
-
-init({Ref, Socket, Transport, Opts}) ->
+init({Ref, Socket, Transport, _Opts}) ->
     ok = ranch:accept_ack(Ref),
-    ok = Transport:setopts(Socket, [{active, once}, {packet, line},
-                                    {keepalive, true}]),
-    gen_server:enter_loop(
-      ?MODULE, [], #state{socket = Socket, transport = Transport}).
+    ok = Transport:setopts(Socket, [{active, once}, {packet, line}, {keepalive, true}]),
+    gen_server:cast(self(), init_session),
+    gen_server:enter_loop(?MODULE, [], #state{socket = Socket, transport = Transport}).
 
 handle_call(_Request, _From, State) ->
 	{reply, ok, State}.
 
-handle_cast(_Msg, State) ->
-	{noreply, State}.
+handle_cast(init_session, State) ->
+    Session = aestratum_session:new(server),
+    Res = aestratum_session:handle_event({conn, init}, Session),
+    result(Res, State).
 
-handle_info({SocketType, Socket, Data}, State) when ?IS_MSG(SocketType) ->
-	{noreply, handle_socket_msg(Socket, Data, State)};
-handle_info({SocketClose, Socket}, State) when ?IS_CLOSE(SocketClose) ->
-	{stop, normal, handle_socket_close(Socket, State)};
-handle_info({SocketError, Socket, Reason}, State) when ?IS_ERROR(SocketError) ->
-	{stop, Reason, handle_socket_error(Socket, State)};
+handle_info({SocketType, _Socket, Data}, State) when ?IS_MSG(SocketType) ->
+    handle_socket_data(Data, State);
+handle_info({SocketClose, _Socket}, State) when ?IS_CLOSE(SocketClose) ->
+	handle_socket_close(State);
+handle_info({SocketError, _Socket, Rsn}, State) when ?IS_ERROR(SocketError) ->
+    handle_socket_error(Rsn, State);
 handle_info(timeout, State) ->
-	{stop, normal, State};
+    handle_socket_timeout(State);
 handle_info(_Info, State) ->
 	{stop, normal, State}.
 
@@ -62,27 +62,45 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% Internal
 
-handle_socket_msg(Socket, Data, #state{socket = Socket,
-                                       transport = Transport,
-                                       session = Session} = State) ->
-	Session1 = process_msg(Data, Session),
-	Transport:setopts(Socket, [{active, once}]),
-    State#state{session = Session1}.
+handle_socket_data(Data, #state{socket = Socket, transport = Transport, session = Session} = State) ->
+	Res = aestratum_session:handle_event({conn, Data}, Session),
+	case is_stop(Res) of
+	    true -> ok;
+	    false -> Transport:setopts(Socket, [{active, once}])
+    end,
+    result(Res, State).
 
-handle_socket_close(_Socket, #state{socket = _Socket} = State) ->
-    %% save in cache
-    State.
+handle_socket_close(#state{session = Session} = State) ->
+    Res = aestratum_session:handle_event({conn, close}, Session),
+    result(Res, State).
 
-handle_socket_error(_Socket, #state{socket = _Socket} = State) ->
-    %% save in cache
-    State.
+handle_socket_error(_Rsn, #state{session = Session} = State) ->
+    %% TODO: log error
+    Res = aestratum_session:handle_event({conn, close}, Session),
+    result(Res, State).
 
-process_msg(Data, State) ->
-    aestratum_session:handle_msg(Data, client_req, State).
-%
-%    case aestratum_session:handle_event(Data, Session) of
-%        {reply, ReplyData, Session1} ->
-%        {noreply, Session1} ->
-%        {stop, Reason, Session1} ->
-%        {stop, Reason, Reply, Session1} ->
-%    end.
+handle_socket_timeout(#state{session = Session} = State) ->
+    Res = aestratim_session:handle_event({conn, timeout}, Session),
+    result(Res, State).
+
+result({send, Rsp, Session}, #state{socket = Socket, transport = Transport} = State) ->
+    send_rsp(Rsp, Socket, Transport),
+    {noreply, State#state{session = Session}};
+result({no_send, Session}, State) ->
+    {noreply, State#state{session = Session}};
+result({stop, Session}, State) ->
+    {stop, normal, State#state{session = Session}}.
+
+is_stop({stop, _Session}) -> true;
+is_stop(_Other) -> false.
+
+send_rsp(Rsp, Socket, Transport) ->
+    ok = Transport:send(Socket, Rsp).
+%    case Transport:send(Socket, Rsp) of
+%        ok ->
+%            ok;
+%        {error, _Rsn} ->
+%            %% TODO: log error
+%            ok
+%    end
+

zzxi/aeternity

apps/aestratum/src/aestratum_handler.erl
@@ -18,39 +18,42 @@
 -record(state, {
           socket,
           transport,
+          module,
           session
          }).
 
--define(IS_MSG(SM), ((SM =:= tcp) or (SM =:= ssl))).
--define(IS_CLOSE(SC), ((SC =:= tcp_closed) or (SC =:= ssl_closed))).
--define(IS_ERROR(SE), ((SE =:= tcp_error) or (SE =:= ssl_error))).
+-define(IS_MSG(T), ((T =:= tcp) or (T =:= ssl))).
+-define(IS_CLOSE(C), ((C =:= tcp_closed) or (C =:= ssl_closed))).
+-define(IS_ERROR(E), ((E =:= tcp_error) or (E =:= ssl_error))).
 
 start_link(Ref, Socket, Transport, Opts) ->
 	{ok, proc_lib:spawn_link(?MODULE, init, [{Ref, Socket, Transport, Opts}])}.
 
 
-
 init({Ref, Socket, Transport, Opts}) ->
     ok = ranch:accept_ack(Ref),
-    ok = Transport:setopts(Socket, [{active, once}, {packet, line},
-                                    {keepalive, true}]),
-    gen_server:enter_loop(
-      ?MODULE, [], #state{socket = Socket, transport = Transport}).
+    ok = Transport:setopts(Socket, [{active, once}, {packet, line}, {keepalive, true}]),
+    Mod = proplists:get_value(module, Opts),
+    gen_server:cast(self(), init_session),
+    gen_server:enter_loop(?MODULE, [], #state{socket = Socket, transport = Transport,
+                                              module = Mod}).
 
 handle_call(_Request, _From, State) ->
 	{reply, ok, State}.
 
-handle_cast(_Msg, State) ->
-	{noreply, State}.
+handle_cast(init_session, #state{module = Mod} = State) ->
+    Session = Mod:new(),
+    Res = Mod:handle_event({conn, init}, Session),
+    result(Res, State).
 
-handle_info({SocketType, Socket, Data}, State) when ?IS_MSG(SocketType) ->
-	{noreply, handle_socket_msg(Socket, Data, State)};
-handle_info({SocketClose, Socket}, State) when ?IS_CLOSE(SocketClose) ->
-	{stop, normal, handle_socket_close(Socket, State)};
-handle_info({SocketError, Socket, Reason}, State) when ?IS_ERROR(SocketError) ->
-	{stop, Reason, handle_socket_error(Socket, State)};
+handle_info({SocketType, _Socket, Data}, State) when ?IS_MSG(SocketType) ->
+    handle_socket_data(Data, State);
+handle_info({SocketClose, _Socket}, State) when ?IS_CLOSE(SocketClose) ->
+	handle_socket_close(State);
+handle_info({SocketError, _Socket, Rsn}, State) when ?IS_ERROR(SocketError) ->
+    handle_socket_error(Rsn, State);
 handle_info(timeout, State) ->
-	{stop, normal, State};
+    handle_socket_timeout(State);
 handle_info(_Info, State) ->
 	{stop, normal, State}.
 
@@ -62,27 +65,45 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% Internal
 
-handle_socket_msg(Socket, Data, #state{socket = Socket,
-                                       transport = Transport,
-                                       session = Session} = State) ->
-	Session1 = process_msg(Data, Session),
-	Transport:setopts(Socket, [{active, once}]),
-    State#state{session = Session1}.
+handle_socket_data(Data, #state{socket = Socket, transport = Transport,
+                                module = Mod, session = Session} = State) ->
+	Res = Mod:handle_event({conn, Data}, Session),
+	case is_stop(Res) of
+	    true -> ok;
+	    false -> Transport:setopts(Socket, [{active, once}])
+    end,
+    result(Res, State).
 
-handle_socket_close(_Socket, #state{socket = _Socket} = State) ->
-    %% save in cache
-    State.
+handle_socket_close(#state{module = Mod, session = Session} = State) ->
+    Res = Mod:handle_event({conn, close}, Session),
+    result(Res, State).
 
-handle_socket_error(_Socket, #state{socket = _Socket} = State) ->
-    %% save in cache
-    State.
+handle_socket_error(_Rsn, #state{module = Mod, session = Session} = State) ->
+    %% TODO: log error
+    Res = Mod:handle_event({conn, close}, Session),
+    result(Res, State).
 
-process_msg(Data, State) ->
-    aestratum_session:handle_msg(Data, client_req, State).
-%
-%    case aestratum_session:handle_event(Data, Session) of
-%        {reply, ReplyData, Session1} ->
-%        {noreply, Session1} ->
-%        {stop, Reason, Session1} ->
-%        {stop, Reason, Reply, Session1} ->
-%    end.
+handle_socket_timeout(#state{module = Mod, session = Session} = State) ->
+    Res = Mod:handle_event({conn, timeout}, Session),
+    result(Res, State).
+
+result({send, Data, #state{module = Mod} = Session},
+       #state{socket = Socket, transport = Transport} = State) ->
+    case send_data(Data, Socket, Transport) of
+        ok ->
+            {noreply, State#state{session = Session}};
+        {error, _Rsn} ->
+            Res = Mod:handle_event({conn, close}, Session),
+            result(Res, State)
+    end;
+result({no_send, Session}, State) ->
+    {noreply, State#state{session = Session}};
+result({stop, Session}, State) ->
+    {stop, normal, State#state{session = Session}}.
+
+is_stop({stop, _Session}) -> true;
+is_stop(_Other) -> false.
+
+send_data(Data, Socket, Transport) ->
+    Transport:send(Socket, Data).
+