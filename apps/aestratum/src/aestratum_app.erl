f4df4615afdb63207ae4f47e1041675388072e64
Juraj Hlista <jurajhlista@gmail.com>
2019-01-31 14:19:40 +0100

    [WIP] Add aestratum_app and aestratum_sup

---
+++ apps/aestratum/src/aestratum_app.erl
@@ -0,0 +1,14 @@
+-module(aestratum_app).
+
+-behavior(application).
+
+-export([start/2,
+         stop/1
+        ]).
+
+start(_Type, _Args) ->
+    aestratum_sup:start_link(server).
+
+stop(_State) ->
+	ok.
+
