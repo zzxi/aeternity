ef388dca7a87ef8b13f03e688a35ff576db990b3
Luca Favatella <luca.favatella@erlang-solutions.com>
2019-02-20 17:34:44 +0100

    XXX Test starting node with old DB with contract txs
    TODO Make test fail for call tx.
    TODO Make test fail for create tx.
    TODO Optional - make test pass (should not fail) for all other txs e.g. force progress tx.
    TODO Revert change in makefile.

--- system_test/common/aest_db_SUITE.erl
+++ system_test/common/aest_db_SUITE.erl
@@ -13,7 +13,8 @@
 -export([
     node_can_reuse_db_of_other_node/1,
     roma_node_can_reuse_db_of_other_roma_node/1,
-    node_can_reuse_db_of_roma_node/1
+    node_can_reuse_db_of_roma_node/1,
+    node_can_reuse_db_of_roma_node_with_contract_txs/1
 ]).
 
 %=== INCLUDES ==================================================================
@@ -31,7 +32,8 @@
 all() -> [
     node_can_reuse_db_of_other_node,
     roma_node_can_reuse_db_of_other_roma_node,
-    node_can_reuse_db_of_roma_node
+    node_can_reuse_db_of_roma_node,
+    node_can_reuse_db_of_roma_node_with_contract_txs
 ].
 
 init_per_suite(Config) ->
@@ -56,25 +58,65 @@ roma_node_can_reuse_db_of_other_roma_node(Cfg) ->
 node_can_reuse_db_of_roma_node(Cfg) ->
     node_can_reuse_db_of_other_node_(fun roma_node_spec/2, fun node_spec/2, Cfg).
 
+node_can_reuse_db_of_roma_node_with_contract_txs(Cfg) ->
+    node_can_reuse_db_of_other_node_(fun roma_node_spec/2, fun node_spec/2, {fun populate_db_with_contract_txs/2, fun assert_db_with_contract_txs_reused/3}, Cfg).
+
 %=== INTERNAL FUNCTIONS ========================================================
 
 node_can_reuse_db_of_other_node_(NodeSpecFun, Cfg) ->
     node_can_reuse_db_of_other_node_(NodeSpecFun, NodeSpecFun, Cfg).
 
 node_can_reuse_db_of_other_node_(CreateDbNodeSpecFun, ReuseDbNodeSpecFun, Cfg) ->
+    node_can_reuse_db_of_other_node_(CreateDbNodeSpecFun, ReuseDbNodeSpecFun, {fun populate_db/2, fun assert_db_reused/3}, Cfg).
+
+node_can_reuse_db_of_other_node_(CreateDbNodeSpecFun, ReuseDbNodeSpecFun, {PopulateDbFun, AssertDbFun}, Cfg) ->
     DbHostPath = node_db_host_path(node1, Cfg),
     N1 = CreateDbNodeSpecFun(node1, DbHostPath),
     N2 = ReuseDbNodeSpecFun(node2, DbHostPath),
     aest_nodes:setup_nodes([N1, N2], Cfg),
     start_and_wait_node(node1, ?STARTUP_TIMEOUT, Cfg),
-    TargetHeight = 3,
-    aest_nodes:wait_for_value({height, TargetHeight}, [node1], TargetHeight * ?MINING_TIMEOUT, Cfg),
-    #{hash := BlockHash} = aest_nodes:get_block(node1, TargetHeight),
+    DbFingerprint = PopulateDbFun(node1, Cfg),
     aest_nodes:stop_node(node1, ?GRACEFUL_STOP_TIMEOUT, Cfg),
     start_and_wait_node(node2, ?STARTUP_TIMEOUT, Cfg),
-    aest_nodes:wait_for_value({height, TargetHeight}, [node2], ?STARTUP_TIMEOUT, Cfg),
-    ?assertMatch({ok, 200, _}, get_block_by_hash(node2, BlockHash)),
+    AssertDbFun(node2, DbFingerprint, Cfg),
     ok.
+
+populate_db(NodeName, Cfg) ->
+    TargetHeight = 3,
+    aest_nodes:wait_for_value({height, TargetHeight}, [NodeName], TargetHeight * ?MINING_TIMEOUT, Cfg),
+    #{hash := BlockHash} = aest_nodes:get_block(node1, TargetHeight),
+    _DbFingerprint = {TargetHeight, BlockHash}.
+
+assert_db_reused(NodeName, {TargetHeight, BlockHash} = _DbFingerprint, Cfg) ->
+    aest_nodes:wait_for_value({height, TargetHeight}, [NodeName], ?STARTUP_TIMEOUT, Cfg),
+    ?assertMatch({ok, 200, _}, get_block_by_hash(NodeName, BlockHash)),
+    ok.
+
+a_fresh_keypair() ->
+    enacl:sign_keypair().
+
+a_fresh_contract_id() ->
+    #{ public := PubKey, secret := _ } = a_fresh_keypair(),
+    aec_id:create(contract, PubKey).
+
+populate_db_with_contract_txs(NodeName, Cfg) ->
+    #{ public := PubKey, secret := PrivKey } = a_fresh_keypair(),
+    CallTxArgs = #{ caller_id   => aec_id:create(account, PubKey)
+        , nonce       => 1
+        , contract_id => a_fresh_contract_id()
+        , fee         => 500000 * aest_nodes:gas_price()
+        , ttl         => 100
+        , abi_version => 1
+        , amount      => 0
+        , gas         => 100
+        , gas_price   => 123456
+        , call_data   => <<"CALL DATA">>
+    },
+    foo = aest_nodes:post_contract_call_tx(NodeName, PrivKey, CallTxArgs),
+    _DbFingerprint = exit(not_yet_implemented).
+
+assert_db_with_contract_txs_reused(NodeName, _DbFingerprint, Cfg) ->
+    exit(not_yet_implemented).
 
 get_block_by_hash(NodeName, Hash) ->
     aest_nodes:request(NodeName, 'GetKeyBlockByHash', #{hash => Hash}).
@@ -94,10 +136,14 @@ format(Fmt, Args) ->
     iolist_to_binary(io_lib:format(Fmt, Args)).
 
 node_spec(Name, DbHostPath) ->
+    node_spec(Name, DbHostPath, true).
+node_spec(Name, DbHostPath, Mining) ->
     DbGuestPath = "/home/aeternity/node/data/mnesia",
-    aest_nodes:spec(Name, [], #{source  => {pull, "aeternity/aeternity:local"}, db_path => {DbHostPath, DbGuestPath}}).
+    aest_nodes:spec(Name, [], #{source  => {pull, "aeternity/aeternity:local"}, db_path => {DbHostPath, DbGuestPath}, mining => #{autostart => Mining}}).
 
 %% Last Roma release.
 roma_node_spec(Name, DbHostPath) ->
+    roma_node_spec(Name, DbHostPath, true).
+roma_node_spec(Name, DbHostPath, Mining) ->
     DbGuestPath = "/home/aeternity/node/data/mnesia",
-    aest_nodes:spec(Name, [], #{source  => {pull, "aeternity/aeternity:v1.4.0"}, db_path => {DbHostPath, DbGuestPath}, config_guest_path => "/home/aeternity/.epoch/epoch/epoch.yaml"}).
+    aest_nodes:spec(Name, [], #{source  => {pull, "aeternity/aeternity:v1.4.0"}, db_path => {DbHostPath, DbGuestPath}, config_guest_path => "/home/aeternity/.epoch/epoch/epoch.yaml", mining => #{autostart => Mining}}).
